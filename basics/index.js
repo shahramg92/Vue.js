b17d6c7162e6cc23e0b30dd3499d172ed4c7c0bd
Shahram Ghassemi <shahramg92@hotmail.com>
2018-08-21 12:42:25 -0500

    styling dynamically

--- basics/index.js
+++ basics/index.js
@@ -1,11 +1,18 @@
 var app = new Vue({
   el: '#app',
   data: {
-    name: 'Shahram'
+    name: 'Shahram',
+    elements: []
   },
   methods: {
     changeName: function() {
       this.name = 'Sofia';
+    },
+    addElement: function() {
+      this.elements.push(this.elements.length + 1);
+    },
+    getColor: function(number) {
+      return number % 2 == 0 ? 'green' : 'red';
     }
   }
 });
